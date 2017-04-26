376ee09ad549ff482b0c188c69d260b79486dfb0
Francisco Rojas <josefcorojas@gmail.com>
2017-04-25 20:12:03 -0600

    add console output to confirm lambda function is running

--- hola.js
+++ hola.js
@@ -5,6 +5,7 @@ var lib = require('lib');
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
+    console.log("Received Event:", event.clickType);
     lib.holaAll(event, function(error, response) {
         return context.succeed(lib.lambdaProxyResponse(200, response));
     });
